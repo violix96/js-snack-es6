14f5634c0dc2b001007994265109ea36555d9c32
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-16 16:42:59 +0200

    [UPD] SNACK-1 completed

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -4,3 +4,45 @@
 // controllo collegamento 
 
 console.log('snack 1, sono connesso')
+
+// creazione di un array di oggetti ogni oggetto rappresenta una bici da corsa 
+
+const bici = [
+    {
+        nome : 'Canyon',
+        peso : 10,
+    },
+    {
+        nome : 'B-M-X',
+        peso : 8,
+    },
+    {
+        nome : 'SuperBike',
+        peso : 12,
+    },
+    {
+        nome : 'Shimano',
+        peso : 7,
+    },
+    {
+        nome : 'Trek',
+        peso : 16,
+    },
+];
+
+console.log(bici);
+
+
+let biciLeggera = bici[0];
+
+
+// ciclo for per iterare su tutte le biciclette dell'array
+for (let i = 0; i < bici.length; i++){
+
+    // se la bici corrente ha un peso minore rispetto a biciLeggera allora aggiorna biciLeggera
+    if ( bici[i].peso < biciLeggera.peso){
+        biciLeggera = bici[i];
+    }
+}
+
+console.log(`La bici più leggera è la ${biciLeggera.nome} con il peso di ${biciLeggera.peso} kg. `);
---
+++ snack1/readme.md
@@ -0,0 +1,7 @@
+Snack 1
+
+Creare un array di oggetti:
+
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+
+Stampare a schermo la bici con peso minore utilizzando template literal.
